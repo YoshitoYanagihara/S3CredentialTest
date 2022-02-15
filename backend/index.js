375f552df2d5510301c992f2d25f5caafbbe0e2f
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-02-16 01:23:49 +0900

    Cookieを複数セットしてみた

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ exports.handler = async (event) => {
             "Access-Control-Allow-Headers" : "Content-Type",
             "Access-Control-Allow-Origin": "http://localhost:8080",
             "Access-Control-Allow-Credentials": true,
-            "Set-Cookie": "Test"
+        },
+        multiValueHeaders: {
+            "Set-Cookie": ["Test1", "Test2", "Test3"],
         },
         body: JSON.stringify({ message: 'CLI Deploy Successful !!' }),
     };
