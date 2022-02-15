8be9f5507789e3f67aaa49999d2619b97edee8db
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-02-15 23:57:57 +0900

    適当にCookieを付加してみるテスト

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+            "Set-Cookie": "Test"
+        },
         body: JSON.stringify('CLI Deploy Successful !!'),
     };
     return response;
