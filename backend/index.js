194f6e7fef436a1d11f2ccf54d0885f103310cd4
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-02-16 01:04:42 +0900

    CORSの設定

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,12 @@ exports.handler = async (event) => {
         statusCode: 200,
         headers: {
             "Content-Type": "application/json",
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "http://localhost:8080",
+            "Access-Control-Allow-Credentials": true,
             "Set-Cookie": "Test"
         },
-        body: JSON.stringify('CLI Deploy Successful !!'),
+        body: JSON.stringify({ message: 'CLI Deploy Successful !!' }),
     };
     return response;
 };
