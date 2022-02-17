c5f52c90e6ebd6a7606c3d43f6a8a462523b970a
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-02-18 00:33:52 +0900

    署名付きCookieを取得して投げるようにした

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,71 @@
-exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "http://localhost:8080",
-            "Access-Control-Allow-Credentials": true,
-        },
-        multiValueHeaders: {
-            "Set-Cookie": ["Test1=Hoge; Path=/hoge", "Test2=Fuga; Path=/hoge/fuga", "Test3=Piyo; Path=/hoge/fuga/piyo.json"],
-        },
-        body: JSON.stringify({ message: 'CLI Deploy Successful !!' }),
-    };
-    response.multiValueHeaders["Set-Cookie"] = response.multiValueHeaders["Set-Cookie"].map(cookie => {
-        cookie += "; Secure; HttpOnly; SameSite=None; Domain=yanap-app.com";
-        return cookie;
+const AWS = require('aws-sdk');
+
+function getPrivateKey () {
+    return new Promise((resolve, reject) => {
+        const secretsManager = new AWS.SecretsManager();
+        secretsManager.getSecretValue({SecretId: "s3_rsa"}, (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data.SecretString);
+        });
     });
-    return response;
+}
+
+function getSignedCookie () {
+    return new Promise(async (resolve, reject) => {
+        const privateKey = await getPrivateKey();
+        const keyPairId = process.env.KEY_PAIR_ID;
+        const signer = new AWS.CloudFront.Signer(keyPairId, privateKey);    
+        signer.getSignedCookie({
+            url: "https://contents.yanap-app.com/hoge/fuga/piyo.json",
+            expires: Math.round(new Date().getTime() / 1000) + 3600,
+        }, (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data);
+        });    
+    });
+}
+
+exports.handler = async (event) => {
+    try {
+        const data = await getSignedCookie();
+        const response = {
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "http://localhost:8080",
+                "Access-Control-Allow-Credentials": true,
+            },
+            multiValueHeaders: {
+                "Set-Cookie": [
+                    `CloudFront-Expires=${data["CloudFront-Expires"]}`,
+                    `CloudFront-Key-Pair-Id=${data["CloudFront-Key-Pair-Id"]}`,
+                    `CloudFront-Signature=${data["CloudFront-Signature"]}`,
+                ],
+            },
+            body: JSON.stringify({ message: "Success" }),
+        };
+        response.multiValueHeaders["Set-Cookie"] = response.multiValueHeaders["Set-Cookie"].map(cookie => {
+            cookie += "; Secure; SameSite=None; Domain=yanap-app.com; Path=/hoge/fuga/piyo.json";
+            return cookie;
+        });
+        return response;
+    } catch (err) {
+        return {
+            statusCode: 500,
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "http://localhost:8080",
+                "Access-Control-Allow-Credentials": true,
+            },
+            body: JSON.stringify({ error: err }),
+        };
+    }
 };
